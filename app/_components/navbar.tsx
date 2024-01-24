905d66d5dbc0023fe60dc61654efcb9fe16990cd
pilladipesh33 <pilladipesh.pd@gmail.com>
2024-01-24 12:32:46 +0530

    package installed

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { MenuContainer } from "@/components/menu-container";
+
+export const Navbar = () => {
+  return (
+    <div className="w-full flex justify-between items-center">
+      <MenuContainer />
+    </div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { MenuContainer } from "@/components/menu-container";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
+import { Navbar } from "./_components/navbar";
 
 export default function Home() {
   return (
     <main className="p-4 h-screen w-full">
-      <MenuContainer />
+      <Navbar />
     </main>
   );
 }
